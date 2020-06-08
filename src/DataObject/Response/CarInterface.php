6fb23074fcff1946b020256536081f45cb6ac11a
Peter Šablica <peter.sablica@mb-e.sk>
2020-06-08 11:05:11 +0200

    Car interface extend data object interface.

--- src/DataObject/Response/CarInterface.php
+++ src/DataObject/Response/CarInterface.php
@@ -4,10 +4,8 @@ declare(strict_types=1);
 
 namespace Teas\AlphaApiClient\DataObject\Response;
 
-interface CarInterface
+use Teas\AlphaApiClient\DataObject\DataObjectInterface;
+
+interface CarInterface extends DataObjectInterface
 {
-    /**
-     * @return string
-     */
-    public function getId(): string;
 }
